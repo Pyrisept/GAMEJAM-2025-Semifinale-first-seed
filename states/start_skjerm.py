ac269cc497f8221be3430991f66474c842d0118f
Axeleration15 <axel@schroder.no>
2025-02-27 12:34:52 +0100

    Jeg har startet å skrive koden til start_skjerm.

---
+++ states/start_skjerm.py
@@ -0,0 +1,10 @@
+# Start skjerm for Rudmon spill.
+
+import pygame, sys
+
+pygame.init()
+
+SCREEN = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Meny(dyrt!)")
+
+BG = pygame.image.load("")
